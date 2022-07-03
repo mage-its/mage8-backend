7e639c9cd59482eaa3a311d3fb1c7d5a3de52bdf
unknown <ramadani.dwicahya@gmail.com>
2022-07-03 21:41:14 +0700

    linkshortener validation

--- src/routes/v1/linkShortener.route.js
+++ src/routes/v1/linkShortener.route.js
@@ -2,15 +2,16 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const linkShortenerController = require('../../controllers/linkShortener.controller');
+const linkShortenerValidation = require('../../validations/linkShortener.validation');
 
 const router = express.Router();
 
-router.post('/', auth('manageLinkShortener'), linkShortenerController.createLinkShortener);
-router.get('/', auth('getLinkShortener'), linkShortenerController.getLinkShorteners);
-router.patch('/:id',auth('manageLinkShortener'), linkShortenerController.updateLinkShortener);
-router.delete('/:id', auth('manageLinkShortener'), linkShortenerController.deleteLinkShortener);
-router.get('/slug/:slug',auth('getLinkShortener'), linkShortenerController.getLinkShortenerBySlug);
-router.patch('/slug/:slug',auth('manageLinkShortener'), linkShortenerController.updateLinkShortenerBySlug);
-router.delete('/slug/:slug',auth('manageLinkShortener'), linkShortenerController.deleteLinkShortenerBySlug);
+router.post('/', auth('manageLinkShortener'), validate(linkShortenerValidation.createLinkShortener), linkShortenerController.createLinkShortener);
+router.get('/', auth('getLinkShortener'), validate(linkShortenerValidation.getLinkShorteners), linkShortenerController.getLinkShorteners);
+router.patch('/:id',auth('manageLinkShortener'), validate(linkShortenerValidation.updateLinkShortener), linkShortenerController.updateLinkShortener);
+router.delete('/:id', auth('manageLinkShortener'), validate(linkShortenerValidation.deleteLinkShortener), linkShortenerController.deleteLinkShortener);
+router.get('/slug/:slug',auth('getLinkShortener'), validate(linkShortenerValidation.getLinkShortenerBySlug), linkShortenerController.getLinkShortenerBySlug);
+router.patch('/slug/:slug',auth('manageLinkShortener'), validate(linkShortenerValidation.updateLinkShortenerBySlug), linkShortenerController.updateLinkShortenerBySlug);
+router.delete('/slug/:slug',auth('manageLinkShortener'), validate(linkShortenerValidation.deleteLinkShortenerBySlug), linkShortenerController.deleteLinkShortenerBySlug);
 
 module.exports = router;
