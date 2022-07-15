870efe07761aa4fce2b3361eed9635caa4d9536f
Akhmad Thoriq <51510460+itstor@users.noreply.github.com>
2022-07-15 20:28:11 +0700

    fix: typo

--- src/middlewares/paymentBarrier.js
+++ src/middlewares/paymentBarrier.js
@@ -37,7 +37,7 @@ const paymentBarrier = async(registeredComp,id) => {
     }
   }
 
-  if(registeredComp === "gameev"){
+  if(registeredComp === "gamedev"){
     try {
       const compe = await getCompeByUserId(id);
       const cabang = compe.kategori === "Siswa" ? "adevs" : "adevm";
