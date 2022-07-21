5e16b4cd4e73c975f67c559962a9413387c404ab
itstor <ahmdthoriq5@gmail.com>
2022-07-22 00:07:18 +0700

    fix: firebase

--- src/config/config.js
+++ src/config/config.js
@@ -41,4 +41,16 @@ module.exports = {
     submitKarya: process.env.CLOSE_SUBMIT_KARYA === 'true',
   },
   rajaOngkirKey: process.env.RAJA_ONGKIR_API_KEY,
+  firebaseAccountKey: {
+    type: process.env.FIREBASE_ACCOUNT_KEY_TYPE,
+    project_id: process.env.FIREBASE_ACCOUNT_KEY_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ACCOUNT_KEY_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ACCOUNT_KEY_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_ACCOUNT_KEY_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ACCOUNT_KEY_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_ACCOUNT_KEY_AUTH_URI,
+    token_uri: process.env.FIREBASE_ACCOUNT_KEY_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_ACCOUNT_KEY_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ACCOUNT_KEY_CLIENT_X509_CERT_URL,
+  },
 };
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,12 @@
 const admin = require('firebase-admin');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const config = require('./config');
 
-const client = new SecretManagerServiceClient();
 // eslint-disable-next-line import/no-unresolved
 
-client
-  .accessSecretVersion({
-    name: `projects/${process.env.GOOGLE_PROJECTID}/secrets/${process.env.SECRET_NAME}/versions/${process.env.SECRET_VERSION}`,
-  })
-  .then(async (response) => {
-    const secret = response[0];
-    const { data } = secret.payload;
-    const secretData = JSON.parse(data);
-    if (config.env !== 'ci') {
-      admin.initializeApp({
-        credential: admin.credential.cert(secretData),
-      });
-    }
+if (config.env !== 'ci') {
+  admin.initializeApp({
+    credential: admin.credential.cert(config.firebaseAccountKey),
   });
+}
 
 module.exports = admin;
